868dafca7c1d7f5dd0dfc8e0f45d9e915b5d38fd
Jason Hannanto <hannanto@usc.edu>
2019-11-19 15:11:22 -0800

    initial commit

---
+++ src/test/java/com/example/projectuniverse/ProjectUniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectuniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUniverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
